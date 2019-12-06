816992cd323593fa5a861b9a034bcf8f8a0e7ca9
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2019-12-06 10:49:02 +0300

    Solution to both puzzles for day 6.

--- 2019/6/6.py
+++ 2019/6/6.py
@@ -12,7 +12,29 @@ def calc_distance_to_com(orbs, sat):
         dist += 1
     return dist
 
+def find_route_to_com(orbs, sat):
+    route = [orbs[sat]]
+    while route[0] != "COM":
+        route.insert(0, orbs[route[0]])
+    return route
+
 checksum = 0
 for satellite in orbits.keys():
     checksum += calc_distance_to_com(orbits, satellite)
 print("checksum = %d" % checksum)    
+
+route_me = find_route_to_com(orbits, "YOU")
+route_santa = find_route_to_com(orbits, "SAN")
+
+while route_me[0] == route_santa[0]:
+    route_me = route_me[1:]
+    route_santa = route_santa[1:]
+    
+# Now the tricky piece. We need to count the number of transfers between nodes,
+# which is N-1 for N nodes. So the total number of transfers to traverse both
+# branches is (len(route_me) -1) + (len(route_santa) -1). Our branches don't
+# include their common node, which adds two more transfers, that are required
+# to switch between branches, thus the total number of transfers is just
+# len(route_me) + len(route_santa)
+
+print("transfers = %d" % (len(route_me) + len(route_santa)))
