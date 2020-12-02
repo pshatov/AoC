3c870a62492be53955782f4deb330e235e4d9a43
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2020-12-02 16:14:44 +0300

    Solution to the second puzzle for day 2

--- 2020/2/2.py
+++ 2020/2/2.py
@@ -7,6 +7,7 @@ with open('input.txt') as f:
     for fl in f:
         DB.append(fl.strip())
 
+
 num_valid = 0
 for row in DB:
     
@@ -17,12 +18,25 @@ for row in DB:
     min, max, char, pwd = m.group(1, 2, 3, 4)
     
     min, max = int(min), int(max)
-    print("%d, %d" % (min, max))
     
     cnt = pwd.count(char)
     if min <= cnt <= max:
         num_valid += 1
         
 print("num_valid: %d" % num_valid)
+
+
+num_valid = 0
+for row in DB:
     
+    m = re.fullmatch(fmt, row)
+    if m is None:
+        raise RuntimeError("Can't parse: '%s'!" % row)
     
+    min, max, char, pwd = m.group(1, 2, 3, 4)
+    
+    min, max = int(min), int(max)
+    if (pwd[min-1] == char) != (pwd[max-1] == char):
+        num_valid += 1
+    
+print("num_valid: %d" % num_valid)
