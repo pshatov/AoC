82f4c4e0f0ade1c49acfdd736778ab48c50cdd05
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2020-12-02 15:55:51 +0300

    Solution to the first puzzle for day 2

---
+++ 2020/2/2.py
@@ -0,0 +1,28 @@
+import re
+
+fmt = re.compile('^(\d+)\-(\d+) ([a-z])\: ([a-z]+).*$')
+
+DB = []
+with open('input.txt') as f:
+    for fl in f:
+        DB.append(fl.strip())
+
+num_valid = 0
+for row in DB:
+    
+    m = re.fullmatch(fmt, row)
+    if m is None:
+        raise RuntimeError("Can't parse: '%s'!" % row)
+    
+    min, max, char, pwd = m.group(1, 2, 3, 4)
+    
+    min, max = int(min), int(max)
+    print("%d, %d" % (min, max))
+    
+    cnt = pwd.count(char)
+    if min <= cnt <= max:
+        num_valid += 1
+        
+print("num_valid: %d" % num_valid)
+    
+    
