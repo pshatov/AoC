e7fb517a6db270162a04d4f26e6ee37ce1bead9e
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2020-12-06 13:00:04 +0300

    Solution to the second puzzle for day 6

--- 2020/6/6.py
+++ 2020/6/6.py
@@ -9,19 +9,36 @@ with open('input.txt') as f:
                 GROUPS.append([])
             GROUPS[-1].append(fls)
             
-GROUPS_YES = []
+GROUPS_ANY = []
 for grp in GROUPS:
     yes = []
     for grp_line in grp:
         for line_ans in grp_line:
             if not line_ans in yes:
                 yes.append(line_ans)
-    GROUPS_YES.append(yes)
+    GROUPS_ANY.append(yes)
     
 sum = 0
-for grp_yes in GROUPS_YES:
+for grp_yes in GROUPS_ANY:
     sum += len(grp_yes)
     
 print("sum: %d" % sum)
             
-        
+GROUPS_ALL = []
+for grp in GROUPS:
+    GROUPS_ALL.append([])
+    for i in range(ord('a'), ord('z')+1):
+        c = chr(i)
+        all = True
+        for grp_line in grp:
+            if not c in grp_line:
+                all = False
+                break
+        if all:
+            GROUPS_ALL.append(c)
+
+sum = 0
+for grp_all in GROUPS_ALL:
+    sum += len(grp_all)
+    
+print("sum: %d" % sum)
