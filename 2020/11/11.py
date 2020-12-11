f180e6a5c7170ec5c200d339eccf921b4d454fac
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2020-12-11 13:30:38 +0300

    Solution to the second puzzle for day 11

--- 2020/11/11.py
+++ 2020/11/11.py
@@ -1,3 +1,5 @@
+import copy
+
 SEAT = []
 NEW_SEAT = []
 
@@ -18,7 +20,7 @@ def print_seats():
         print("")
     print("")
 
-
+    
 def patch_seats():
     for y in range(len(SEAT)):
         SEAT[y].insert(0, '.')
@@ -31,7 +33,7 @@ def patch_seats():
             NEW_SEAT[y].append(SEAT[y][x])
 
 
-def calc_adj_seats(x, y):
+def calc_adj_seats1(x, y):
     tot = 0
     if SEAT[y-1][x-1] == '#': tot += 1
     if SEAT[y  ][x-1] == '#': tot += 1
@@ -43,13 +45,41 @@ def calc_adj_seats(x, y):
     if SEAT[y+1][x+1] == '#': tot += 1
     return tot
 
-def mutate_seats():
+
+def calc_adj_seats2_dir(x0, y0, dx, dy):
+    x = x0
+    y = y0
+    while True:
+        x += dx
+        y += dy
+        if y == 0: return 0
+        if x == 0: return 0
+        if y == (len(SEAT)-1): return 0
+        if x == (len(SEAT[y])-1): return 0
+        if SEAT[y][x] == '#': return 1
+        elif SEAT[y][x] == 'L': return 0
+
+
+def calc_adj_seats2(x, y):
+    tot = 0
+    tot += calc_adj_seats2_dir(x, y, -1, -1)
+    tot += calc_adj_seats2_dir(x, y,  0, -1)
+    tot += calc_adj_seats2_dir(x, y, +1, -1)
+    tot += calc_adj_seats2_dir(x, y, -1,  0)
+    tot += calc_adj_seats2_dir(x, y, +1,  0)
+    tot += calc_adj_seats2_dir(x, y, -1, +1)
+    tot += calc_adj_seats2_dir(x, y,  0, +1)
+    tot += calc_adj_seats2_dir(x, y, +1, +1)
+    return tot
+    
+
+def mutate_seats1():
     chg = 0
     for y in range(1, len(SEAT)-1):
         for x in range(1, len(SEAT[y])-1):
             s = SEAT[y][x]
             s_new = s
-            n = calc_adj_seats(x, y)
+            n = calc_adj_seats1(x, y)
             if s == 'L' and n == 0: s_new = '#'
             if s == '#' and n >= 4: s_new = 'L'
             NEW_SEAT[y][x] = s_new
@@ -61,22 +91,55 @@ def mutate_seats():
     return chg
 
 
+def mutate_seats2():
+    chg = 0
+    for y in range(1, len(SEAT)-1):
+        for x in range(1, len(SEAT[y])-1):
+            s = SEAT[y][x]
+            s_new = s
+            n = calc_adj_seats2(x, y)
+            if s == 'L' and n == 0: s_new = '#'
+            if s == '#' and n >= 5: s_new = 'L'
+            NEW_SEAT[y][x] = s_new
+            
+    for y in range(1, len(SEAT)-1):
+        for x in range(1, len(SEAT[y])-1):
+            if NEW_SEAT[y][x] != SEAT[y][x]:
+                chg += 1
+            SEAT[y][x] = NEW_SEAT[y][x]
+    return chg
+    
+    
 def main():
     load_seats()
     patch_seats()
     
+    _S = copy.deepcopy(SEAT)
+    
     while True:
-        n = mutate_seats()
+        n = mutate_seats1()
         if n == 0:
             break
-            
+
+    tot = 0
+    for y in range(1, len(SEAT)-1):
+        for x in range(1, len(SEAT[y])-1):
+            if SEAT[y][x] == '#': tot += 1
+            SEAT[y][x] = _S[y][x] # !
+    print("tot: %d" % tot)
+
+    while True:
+        n = mutate_seats2()
+        if n == 0:
+            break
+
     tot = 0
     for y in range(1, len(SEAT)-1):
         for x in range(1, len(SEAT[y])-1):
             if SEAT[y][x] == '#': tot += 1
     print("tot: %d" % tot)
-
-
+    
+    
 if __name__ == '__main__':
     main()
             
