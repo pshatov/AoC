a1425a3b584a1d1470b23689f42d3ba2306f84e5
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2020-12-30 14:48:53 +0300

    WIP on day 23

--- 2020/23/23.py
+++ 2020/23/23.py
@@ -1,4 +1,6 @@
-ROUNDS = 100
+ROUNDS = 200
+
+NUM_CUPS = 20
 
 LABELS = [9, 6, 3, 2, 7, 5, 4, 8, 1]
 #LABELS = [3, 8, 9, 1, 2, 5, 4, 6, 7]
@@ -8,6 +10,8 @@ class Cups:
 
     def __init__(self, labels):
         self._labels = labels[:]
+        while len(self._labels) < NUM_CUPS:
+            self._labels.append(len(self._labels)+1)
         self._current_label = labels[0]
 
     @property
@@ -16,20 +20,20 @@ class Cups:
 
     def _print_cups(self):
         print("cups: ", end='')
-        for i in range(9):
-            fmt = "(%d)" if i == self._current_index else " %d "
+        for i in range(NUM_CUPS):
+            fmt = "(%3d)" if i == self._current_index else " %3d "
             print(fmt % self._labels[i], end='')
-        print()
+        #print()
 
     def round(self):
 
         self._print_cups()
 
-        x = self._labels.pop((self._current_index + 1) % 9)
-        y = self._labels.pop((self._current_index + 1) % 8)
-        z = self._labels.pop((self._current_index + 1) % 7)
+        x = self._labels.pop((self._current_index + 1) % NUM_CUPS)
+        y = self._labels.pop((self._current_index + 1) % (NUM_CUPS-1))
+        z = self._labels.pop((self._current_index + 1) % (NUM_CUPS-2))
 
-        print("pick up: %d, %d, %d" % (x, y, z))
+        #print("pick up: %d, %d, %d" % (x, y, z))
 
         destination_label = self._current_label - 1
         while True:
@@ -41,15 +45,15 @@ class Cups:
                 continue
             break
 
-        print("destination: %d" % destination_label)
+        #print("destination: %d" % destination_label)
 
         self._labels.insert(self._labels.index(destination_label)+1, z)
         self._labels.insert(self._labels.index(destination_label)+1, y)
         self._labels.insert(self._labels.index(destination_label)+1, x)
 
-        self._current_label = self._labels[(self._current_index + 1) % 9]
+        self._current_label = self._labels[(self._current_index + 1) % NUM_CUPS]
 
-        print()
+        #print()
 
     def order(self):
         t = self._labels[:]
@@ -57,24 +61,18 @@ class Cups:
             t0 = t.pop(0)
             t.append(t0)
         o = ""
-        for i in range(1, 9):
+        for i in range(1, NUM_CUPS):
             o += str(t[i])
         return o
-
-
-    #
-    #The crab places the cups it just picked up so that they are immediately clockwise of the destination cup. They keep the same order as when they were picked up.
-    #The crab selects a new current cup: the cup which is immediately clockwise of the current cup.
-
-    
 
 
 def main():
 
     cups = Cups(LABELS)
     for r in range(ROUNDS):
-        print("-- move %d --" % (r+1))
+        #print("-- move %d --" % (r+1), flush=True)
         cups.round()
+        print ("-- %d" % (r+1))
     print(cups.order())
 
 
