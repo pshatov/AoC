bd0b77fedca2ee47e6d23e21453f646aad191e40
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2021-12-01 18:23:25 +0300

    Small optimization

--- 2021/1/1.py
+++ 2021/1/1.py
@@ -1,17 +1,7 @@
 from typing import List
 
 
-def part1(depths: List[int]) -> int:
-
-    num_inc = 0
-    for i in range(1, len(depths)):
-        if depths[i] > depths[i-1]:
-            num_inc += 1
-
-    return num_inc
-
-
-def part2(depths: List[int], wnd_len=3) -> int:
+def part12(depths: List[int], wnd_len=1) -> int:
 
     num_inc = 0
     for i in range(wnd_len, len(depths)):
@@ -28,10 +18,10 @@ def main() -> None:
 
     depths = [int(fl.strip()) for fl in f_lines]
 
-    num_inc1 = part1(depths)
+    num_inc1 = part12(depths)
     print(f"num_inc1 == {num_inc1}")
 
-    num_inc2 = part2(depths)
+    num_inc2 = part12(depths, wnd_len=3)
     print(f"num_inc2 == {num_inc2}")
 
 
