afe31112e5e7b7c36726ae083efac93768790091
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2021-12-14 11:29:51 +0300

    Solution to the second puzzle for day 13

--- 2021/13/13.py
+++ 2021/13/13.py
@@ -58,7 +58,48 @@ def _fold_paper_along_x(paper: Paper, fold_x: int) -> None:
 
 # ---------------------------------------------------------------------------------------------------------------------
 def _fold_paper_along_y(paper: Paper, fold_y: int) -> None:
-    pass
+
+    new_paper: Dict[int, List[bool]]
+    new_paper = {}
+
+    for y in range(fold_y):
+        new_paper[y] = paper[y][:]
+
+    for y in range(fold_y + 1, len(paper)):
+        y_new = 2 * fold_y - y
+        if y_new not in new_paper.keys():
+            new_paper[y_new] = paper[y][:]
+        else:
+            for x in range(len(paper[y])):
+                new_paper[y_new][x] = new_paper[y_new][x] or paper[y][x]
+
+    paper.clear()
+    for y in sorted(new_paper.keys()):
+        paper.append(new_paper[y][:])
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+def print_line() -> None:
+    for x in range(8):
+        if x == 0:
+            print('+', end='')
+        for xx in range(5):
+            print('-', end='')
+        print('+', end='')
+    print()
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+def print_paper(paper: Paper, y: int) -> None:
+    for x in range(8):
+        if x == 0:
+            print('|', end='')
+        for xx in range(5):
+            print('X' if paper[y][5 * x + xx] else ' ', end='')
+        print('|', end='')
+    print()
 # ---------------------------------------------------------------------------------------------------------------------
 
 
@@ -92,6 +133,15 @@ def main() -> None:
     fold_paper(paper, folds[0])
     num_dots = sum(sum(t) for t in paper)
     print("part 1: %d" % num_dots)
+
+    for f in range(1, len(folds)):
+        fold_paper(paper, folds[f])
+
+    print("part 2:")
+    print_line()
+    for y in range(len(paper)):
+        print_paper(paper, y)
+    print_line()
 # ---------------------------------------------------------------------------------------------------------------------
 
 
