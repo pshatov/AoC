5ae7a5d8b3bacfbcc9e5a481e70693260aca3540
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2022-12-03 12:14:28 +0300

    Solution to the second puzzle for day 3

--- 2022/3/3.py
+++ 2022/3/3.py
@@ -8,6 +8,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # Imports
 # ---------------------------------------------------------------------------------------------------------------------
+from typing import List
 
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -23,6 +24,20 @@ def find_wrong_item_type(items: str) -> str:
     assert len(wrong_items) == 1
 
     return list(wrong_items)[0]
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+def find_common_item_type(items: List[str]) -> str:
+
+    items_x = set(items[0])
+    items_y = set(items[1])
+    items_z = set(items[2])
+
+    common_items = items_x.intersection(items_y).intersection(items_z)
+    assert len(common_items) == 1
+
+    return list(common_items)[0]
 # ---------------------------------------------------------------------------------------------------------------------
 
 
@@ -50,9 +65,16 @@ def main() -> None:
         wrong_type = find_wrong_item_type(next_line)
         wrong_type_priority = get_item_type_priority(wrong_type)
         total_priority += wrong_type_priority
-
     print("part 1: %d" % total_priority)
 
+    assert len(all_lines) % 3 == 0
+
+    total_priority = 0
+    for i in range(0, len(all_lines), 3):
+        common_type = find_common_item_type(all_lines[i: i + 3])
+        common_type_priority = get_item_type_priority(common_type)
+        total_priority += common_type_priority
+    print("part 2: %d" % total_priority)
 # ---------------------------------------------------------------------------------------------------------------------
 
 
