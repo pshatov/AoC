921c1ec27eadba29c911a88f870d01dbb76954c6
Pavel Shatov <48762813+pshatov@users.noreply.github.com>
2022-12-13 13:10:22 +0300

    Solution to the second puzzle for day 13

--- 2022/13/13.py
+++ 2022/13/13.py
@@ -8,7 +8,13 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # Imports
 # ---------------------------------------------------------------------------------------------------------------------
-from typing import List, Optional, Union
+from functools import cmp_to_key
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# More Imports
+# ---------------------------------------------------------------------------------------------------------------------
+from typing import Union, List
 
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -29,53 +35,33 @@ class Pair:
 
 
 # ---------------------------------------------------------------------------------------------------------------------
-def str_no_spaces(value: ListOrInt) -> str:
-    return str(value).replace(' ', '')
-# ---------------------------------------------------------------------------------------------------------------------
-
-
-# ---------------------------------------------------------------------------------------------------------------------
-def print_level(msg: str, level: int) -> None:
-    print("%s- %s" % ("  " * level, msg))
-# ---------------------------------------------------------------------------------------------------------------------
-
-
-# ---------------------------------------------------------------------------------------------------------------------
-def compare_order(left_part: ListOrInt, right_part: ListOrInt, level: Optional[int] = 0) -> int:
-
-    print_level("Compare %s vs %s" % (str_no_spaces(left_part), str_no_spaces(right_part)), level)
+def compare_order(left_part: ListOrInt, right_part: ListOrInt) -> int:
 
     if isinstance(left_part, int) and isinstance(right_part, int):
         if left_part < right_part:
-            print_level("Left side is smaller, so inputs are in the right order", level + 1)
             return -1
         elif left_part == right_part:
             return 0
         else:
-            print_level("Right side is smaller, so inputs are not in the right order", level + 1)
             return 1
     elif isinstance(left_part, list) and isinstance(right_part, list):
         index = 0
         while index < min(len(left_part), len(right_part)):
-            correct_order = compare_order(left_part[index], right_part[index], level + 1)
+            correct_order = compare_order(left_part[index], right_part[index])
             if correct_order != 0:
                 return correct_order
             index += 1
         if len(left_part) < len(right_part):
-            print_level("Left side ran out of items, so inputs are in the right order", level)
             return -1
         elif len(left_part) == len(right_part):
             return 0
         else:
-            print_level("Right side ran out of items, so inputs are not in the right order", level)
             return 1
     elif isinstance(left_part, list) and isinstance(right_part, int):
         right_part = [right_part]
-        print_level("Mixed types; convert right to %s and retry comparison" % str_no_spaces(right_part), level + 1)
         return compare_order(left_part, right_part)
     elif isinstance(left_part, int) and isinstance(right_part, list):
         left_part = [left_part]
-        print_level("Mixed types; convert left to %s and retry comparison" % str_no_spaces(left_part), level + 1)
         return compare_order(left_part, right_part)
     else:
         raise RuntimeError
@@ -103,7 +89,6 @@ def main() -> None:
     result = 0
     for i in range(len(all_pairs)):
         i1 = i + 1
-        print("== Pair %d ==" % i1)
         next_pair = all_pairs[i]
         correct_order = compare_order(next_pair.left_part, next_pair.right_part)
         if correct_order == -1:
@@ -115,6 +100,24 @@ def main() -> None:
     for next_pair in all_pairs:
         all_signals.append(next_pair.left_part)
         all_signals.append(next_pair.right_part)
+
+    all_signals_sorted = sorted(all_signals, key=cmp_to_key(compare_order))
+    index, index_divider_2, index_divider_6 = 1, None, None
+    for next_signal in all_signals_sorted:
+        if len(next_signal) == 1:
+            if isinstance(next_signal[0], list):
+                if len(next_signal[0]) == 1:
+                    if isinstance(next_signal[0][0], int):
+                        if next_signal[0][0] == 2:
+                            index_divider_2 = index
+                        elif next_signal[0][0] == 6:
+                            index_divider_6 = index
+        index += 1
+
+    assert index_divider_2 is not None
+    assert index_divider_6 is not None
+
+    print("part 2: %d" % (index_divider_2 * index_divider_6))
 # ---------------------------------------------------------------------------------------------------------------------
 
 
